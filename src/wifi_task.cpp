09af20652c6e0d45c9ce340ca27053f2ac990115
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-03-02 22:14:58 +0100

    Update WiFi connection method with constants

--- src/wifi_task.cpp
+++ src/wifi_task.cpp
@@ -1,13 +1,25 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "config.h"
+
+extern TaskHandle_t wifiTaskHandle;
+
 void wifiTask(void *parameter) {
-    WiFi.begin("SSID", "PASSWORD");
+
+    Serial.println("WiFi Task Started");
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     while (WiFi.status() != WL_CONNECTED) {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        Serial.println("Connecting to WiFi...");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
     Serial.println("WiFi Connected");
+    Serial.println(WiFi.localIP());
 
     while (true) {
+        // Keep connection alive
         vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
